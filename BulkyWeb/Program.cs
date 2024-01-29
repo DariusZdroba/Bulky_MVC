a6089bb60ab13f1fd5e65e390ec9428b7a100724
Darius <dariuszdroba@gmail.com>
2024-01-29 17:07:27 +0200

    NULL

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>(); 
 
 var app = builder.Build();
-
+//
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
